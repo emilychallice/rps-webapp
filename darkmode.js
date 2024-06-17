e534cd507f2b7a1879fc86b447509ce93be4cc36
Emily Crawley <72890369+emilychallice@users.noreply.github.com>
2024-06-16 20:44:46 -0400

    Add dark mode
    Working toggleable dark mode button that switches the main bg-color var between bg-colour-lightmode and bg-colour-darkmode defined in the CSS :root.

---
+++ darkmode.js
@@ -0,0 +1,23 @@
+const darkModeButton = document.querySelector("#darkmode-button");
+
+darkModeButton.addEventListener("click", darkModeFunction);
+
+function darkModeFunction()
+{
+  if (darkModeButton.classList.contains("active"))
+  {
+    // Toggle light mode
+    darkModeButton.classList.remove("active");
+    darkModeButton.textContent = "Dark Mode";
+
+    document.documentElement.style.setProperty("--bg-colour", "var(--bg-colour-lightmode)");
+
+  }
+  else
+  { // Toggle dark mode
+    darkModeButton.classList.add("active");
+    darkModeButton.textContent = "Light Mode";
+
+    document.documentElement.style.setProperty("--bg-colour", "var(--bg-colour-darkmode)");
+  }
+}
