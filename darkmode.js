acfd53c7b87dce47d7b776057d9fb7247e3cabe5
Emily Crawley <72890369+emilychallice@users.noreply.github.com>
2024-06-16 20:58:21 -0400

    Clean up darkmode.js

--- darkmode.js
+++ darkmode.js
@@ -5,19 +5,15 @@ darkModeButton.addEventListener("click", darkModeFunction);
 function darkModeFunction()
 {
   if (darkModeButton.classList.contains("active"))
-  {
-    // Toggle light mode
+  { // Toggle light mode
     darkModeButton.classList.remove("active");
     darkModeButton.textContent = "Dark Mode";
-
     document.documentElement.style.setProperty("--bg-colour", "var(--bg-colour-lightmode)");
-
   }
   else
   { // Toggle dark mode
     darkModeButton.classList.add("active");
     darkModeButton.textContent = "Light Mode";
-
     document.documentElement.style.setProperty("--bg-colour", "var(--bg-colour-darkmode)");
   }
 }
